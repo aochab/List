bdd1291e498ec66c0caaf1deb92efd04bb6c004a
aochab <47316062+aochab@users.noreply.github.com>
2019-04-03 16:12:22 +0200

    Update list.h

--- list.h
+++ list.h
@@ -5,7 +5,7 @@
 
 typedef struct tagListItem
 {
-	LISTINFO* m_pInfo;		//pierwsze pole jest wskaünikiem do pola nKey w global.h
+	LISTINFO* m_pInfo;		//pierwsze pole jest wska≈∫nikiem do pola nKey w global.h
 	tagListItem* m_pNext;
 } ListItem;
 
@@ -19,10 +19,10 @@ ListItem* Front( ListItem* pList );
 void FreeList( ListItem* pList );
 
 ListItem* Find( ListItem* pList, const void* search, ListItem** pPrev, 
-		int( __cdecl* compare )( const void* p, const void* v ) ); //wskaünik do szukanego,const void*, pPrev, wskaünik do funkcji otrzymujπcej dwa parametry const void* i zwracajπcej int
+		int( __cdecl* compare )( const void* p, const void* v ) ); //wska≈∫nik do szukanego,const void*, pPrev, wska≈∫nik do funkcji otrzymujƒÖcej dwa parametry const void* i zwracajƒÖcej int
 
-void SortList( ListItem* pList, int ( __cdecl* compare )( const void* p, const void* v ) );	//wskaünik do funkcji otrzymujπcej dwa parametry const void* i zwracajπcej int
+void SortList( ListItem* pList, int ( __cdecl* compare )( const void* p, const void* v ) );	//wska≈∫nik do funkcji otrzymujƒÖcej dwa parametry const void* i zwracajƒÖcej int
 
 #endif
- //__cdecl domyúlna konwencja wywo≥ywania programÛw C i C++, 
+
 
